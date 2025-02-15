7ac404a682496d117f5ed697540ab02bba35ff30
Luiti_LP <89021748+MeloCuentan@users.noreply.github.com>
2025-02-15 15:52:00 +0000

    Add files via upload

---
+++ src/CST820.cpp
@@ -0,0 +1,79 @@
+#include "L_CST820.h"
+
+// Constructor
+CST820::CST820(uint8_t sda, uint8_t scl, uint32_t frecuencia)
+{
+    _sda = sda;
+    _scl = scl;
+    _frecuencia = frecuencia;
+    _x = 0;
+    _y = 0;
+    _rotacion = 0;
+}
+
+// Inicializa el puerto I2C
+void CST820::begin(uint16_t ancho, uint16_t alto)
+{
+    Wire.begin(_sda, _scl, _frecuencia);
+    _ancho = ancho;
+    _alto = alto;
+}
+
+// Establecer orientacion de la pantalla
+void CST820::setRotation(uint8_t rotacion) {
+  if (rotacion > 3) return;
+  _rotacion = rotacion;
+}
+
+// Método para leer el estado táctil
+bool CST820::isPressed()
+{
+    static const uint8_t bytes = 7;
+    Wire.beginTransmission(_ADDR_TOUCH);
+    Wire.write(0x00);
+
+    if (Wire.endTransmission(false) != 0)
+        return false;
+
+    Wire.requestFrom((int)_ADDR_TOUCH, (int)bytes);
+    if (Wire.available() != bytes)
+        return false;
+
+    uint8_t data[bytes];
+    for (uint8_t i = 0; i < bytes; i++)
+    {
+        data[i] = Wire.read(); // Guardamos valores leídos
+    }
+
+    if (data[2] == 0x01)
+    {                                  // Si se ha detectado una pulsación
+        _x = (data[5] << 8) | data[6]; // Guardamos valor (0 - 320)
+        _y = 240 - data[4];            // Guardamos valor (0 - 240)
+        return true;
+    }
+    return false;
+}
+
+// Método para obtener las coordenadas X e Y
+void CST820::getCoordinates(uint16_t &outX, uint16_t &outY)
+{
+    switch (_rotacion)
+    {
+    case 0:
+        outX = _alto - _y;
+        outY = _x;
+        break;
+    case 1:
+        outX = _x;
+        outY = _y;
+        break;
+    case 2:
+        outX = _y;
+        outY = _ancho - _x;
+        break;
+    case 3:
+        outX = _ancho - _x;
+        outY = _alto - _y;
+        break;
+    }
+}
---
+++ src/CST820.h
@@ -0,0 +1,25 @@
+// CST820.h
+#ifndef CST820_H
+#define CST820_H
+
+#include <Wire.h>
+
+class CST820
+{
+public:
+    CST820(uint8_t sda, uint8_t scl, uint32_t frecuencia); // Constructor
+    void begin(uint16_t ancho = 320, uint16_t alto = 240);                                          // Inicializa el puerto I2C
+    void setRotation(uint8_t rotacion);                    // Rotacion del táctil
+    bool isPressed();                                      // Método para leer el estado táctil
+    void getCoordinates(uint16_t &outX, uint16_t &outY);   // Método para obtener las coordenadas X e Y
+
+private:
+    const uint8_t _ADDR_TOUCH = 0x15; // Dirección I2C del CST820
+    uint8_t _sda, _scl;               // Pines del bus I2C
+    uint8_t _rotacion;             // Orientacion de la pantalla
+    uint16_t _x, _y;                  // Coordenadas de la detección del táctil
+    uint16_t _ancho, _alto;
+    uint32_t _frecuencia;             // Frecuencia del bus I2C
+};
+
+#endif
